d976b5b5fb6e991a59d8c4c0b193bad04b0f37ba
Sebastian Drost <s.drost@52north.org>
2022-02-23 19:09:59 +0100

    Enhance normalizing unit tests

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -15,10 +15,27 @@ def create_streamflow_data():
     return df
 
 
+def create_streamflow_data_for_fitting():
+    dates = pd.date_range("2021", periods=20)
+    values = np.arange(5, 101, 5)
+
+    df = pd.DataFrame({"streamflow": values}, index=dates)
+    return df
+
+
 def create_forcings_data():
     dates = pd.date_range("2021", periods=20)
     temp_values = np.random.uniform(low=0, high=10, size=(20,))
     prcp_values = np.random.uniform(low=0, high=10, size=(20,))
+
+    df = pd.DataFrame({"temp": temp_values, "prcp": prcp_values}, index=dates)
+    return df
+
+
+def create_forcings_data_for_fitting():
+    dates = pd.date_range("2021", periods=20)
+    prcp_values = np.arange(3, 61, 3)
+    temp_values = np.arange(4, 81, 4)
 
     df = pd.DataFrame({"temp": temp_values, "prcp": prcp_values}, index=dates)
     return df
@@ -38,7 +55,7 @@ class TestLumpedDataset(unittest.TestCase):
         self.__dataset = dataset.LumpedDataset(ds_timeseries, ["temp", "prcp"], ["streamflow"],
                                                "2021-01-01", "2021-01-20")
 
-    def test_normalize_single(self):
+    def test_normalize(self):
         min_prcp_index = self.__dataset.timeseries.prcp.argmin(dim=["time"])
         max_prcp_index = self.__dataset.timeseries.prcp.argmax(dim=["time"])
         min_streamflow_index = self.__dataset.timeseries.streamflow.argmin(dim=["time"])
@@ -58,3 +75,41 @@ class TestLumpedDataset(unittest.TestCase):
         self.assertEqual(self.__dataset.timeseries.prcp.isel(min_prcp_index), 0)
         self.assertEqual(self.__dataset.timeseries.prcp.isel(max_prcp_index), 1)
 
+    def test_normalize_single(self):
+        min_prcp = self.__dataset.timeseries.prcp.min()
+        max_prcp = self.__dataset.timeseries.prcp.max()
+        min_temp = self.__dataset.timeseries.temp.min()
+        max_temp = self.__dataset.timeseries.temp.max()
+        min_streamflow = self.__dataset.timeseries.streamflow.min()
+        max_streamflow = self.__dataset.timeseries.streamflow.max()
+
+        min_prcp_fit = 3
+        max_prcp_fit = 60
+        min_temp_fit = 4
+        max_temp_fit = 80
+        min_streamflow_fit = 5
+        max_streamflow_fit = 100
+
+        forcings_fit = create_forcings_data_for_fitting()
+        streamflow_fit = create_streamflow_data_for_fitting()
+        ds_timeseries_fit = xr.Dataset.from_dataframe(forcings_fit.join(streamflow_fit, how="outer"))
+
+        self.__dataset.normalize(ds_timeseries_fit.min(), ds_timeseries_fit.max())
+
+        prcp_min_exp = (min_prcp - min_prcp_fit) / (max_prcp_fit - min_prcp_fit)
+        self.assertEqual(prcp_min_exp, self.__dataset.timeseries.prcp.min().values)
+
+        prcp_max_exp = (max_prcp - min_prcp_fit) / (max_prcp_fit - min_prcp_fit)
+        self.assertEqual(prcp_max_exp, self.__dataset.timeseries.prcp.max().values)
+
+        temp_min_exp = (min_temp - min_temp_fit) / (max_temp_fit - min_temp_fit)
+        self.assertEqual(temp_min_exp, self.__dataset.timeseries.temp.min().values)
+
+        temp_max_exp = (max_temp - min_temp_fit) / (max_temp_fit - min_temp_fit)
+        self.assertEqual(temp_max_exp, self.__dataset.timeseries.temp.max().values)
+
+        streamflow_min_exp = (min_streamflow - min_streamflow_fit) / (max_streamflow_fit - min_streamflow_fit)
+        self.assertEqual(streamflow_min_exp, self.__dataset.timeseries.streamflow.min().values)
+
+        streamflow_max_exp = (max_streamflow - min_streamflow_fit) / (max_streamflow_fit - min_streamflow_fit)
+        self.assertEqual(streamflow_max_exp, self.__dataset.timeseries.streamflow.max().values)
