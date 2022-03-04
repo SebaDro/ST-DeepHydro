8074c912537f723c7d54cfd807dd334205460609
Sebastian Drost <s.drost@52north.org>
2022-03-04 13:49:53 +0100

    Refine NaN handling

--- libs/models.py
+++ libs/models.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 import xarray as xr
 from libs import config
 from libs import dataset
+from libs import generator
 from libs import monitoring
-from libs import processing
 
 
 class AbstractModel:
@@ -91,7 +91,7 @@ class AbstractModel:
         test_gen = self.__create_timeseries_generator(test_ds)
         return self.__model.evaluate(test_gen, return_dict=True)
 
-    def predict(self, ds: dataset.AbstractDataset, basin: str, as_dataset: bool = True):
+    def predict(self, ds: dataset.AbstractDataset, basin: str, as_dataset: bool = True, remove_nan: bool = False):
         """
         Uses the trained model the calculate predictions for the given dataset.
 
@@ -103,25 +103,28 @@ class AbstractModel:
             Basin ID
         as_dataset: bool
             Indicates if model predictions should be returned as raw numpy.ndarray or as xarray.Dataset
+        remove_nan: bool
+            Indicates if the timeseries generator should remove timesteps which contains NaN values for target
+            variables. Default is false, since input targets does not matter for calculating predictions.
 
         Returns
         -------
             Model predictions
 
         """
-        gen = self.__create_timeseries_generator(ds)
+        gen = self.__create_timeseries_generator(ds, remove_nan)
         predictions = self.__model.predict(gen)
         if as_dataset:
-            return self.to_dataset(ds, predictions, basin)
+            return self.prediction_to_dataset(ds, predictions, basin, remove_nan)
         else:
             return predictions
 
-    def prediction_to_dataset(self, ds: dataset.AbstractDataset, predictions: np.ndarray, basin: str) -> xr.Dataset:
+    def prediction_to_dataset(self, ds: dataset.AbstractDataset, predictions: np.ndarray, basin: str,
+                              remove_nan: bool = False) -> xr.Dataset:
         """
         Creates a xarray.Dataset for raw model predictions. Therefore, the model outputs and the dataset that has been
         used as model input for calculating the predictions are aligned. The resulting xarrary.Dataset has the same
-        coordinate dimensions as the input dataset but takes into account start and end date of the target variables
-        as well as NaN values.
+        coordinate dimensions as the input dataset. NaN values may be optionally removed.
 
         Parameters
         ----------
@@ -131,20 +134,28 @@ class AbstractModel:
             Raw model output
         basin: str
             Basin ID
+        remove_nan: bool
+            Indicates if timesteps which contain NaN values for the target variables in the input dataset should be
+            preserved or not. If true, the resulting xarray.Dataset only contains those timesteps, which do not
+            contain NaN values input dataset. Note, that this flag should be set in accordance to the flag that has been
+            set for the model prediction method.
 
         Returns
         -------
         Model predictions as xarray.Dataset
 
         """
-        target_start_date = np.datetime64(ds.start_date) + np.timedelta64(self._config.timesteps, 'D') \
-                            + np.timedelta64(self._config.offset, 'D') - np.timedelta64(1, 'D')
+        target_start_date = np.datetime64(ds.start_date) + np.timedelta64(self._config.timesteps, 'D') + np.timedelta64(
+            self._config.offset, 'D') - np.timedelta64(1, 'D')
         res_ds = ds.timeseries.sel(time=slice(target_start_date, np.datetime64(ds.end_date)))
 
         res_dict = {}
         for i, param in enumerate(ds.target_cols):
-            non_nan_flags = np.invert(np.isnan(res_ds.sel(basin=basin)[param]))
-            res_times = res_ds.time[non_nan_flags]
+            if remove_nan:
+                non_nan_flags = np.invert(np.isnan(res_ds.sel(basin=basin)[param]))
+                res_times = res_ds.time[non_nan_flags]
+            else:
+                res_times = res_ds.time
             res_dict[param] = xr.DataArray(predictions[:, i], coords=[res_times], dims=["time"])
         ds_prediction = xr.Dataset(res_dict)
         ds_prediction = ds_prediction.assign_coords({"basin": basin})
