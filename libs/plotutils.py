171b66d14eeab3f8f41eeb6ec481de6be16472fd
Sebastian Drost <s.drost@52north.org>
2022-04-05 17:50:51 +0200

    Add documentation

--- libs/plotutils.py
+++ libs/plotutils.py
@@ -3,6 +3,15 @@ import tensorflow as tf
 
 
 def plot_loss(history: tf.keras.callbacks.History):
+    """
+    Visualizes the progress of a trained model by plotting the loss per epoch
+
+    Parameters
+    ----------
+    history: tf.keras.callbacks.History
+        A Tensorflow history object that holds information about training progress
+
+    """
     plt.plot(history.history['loss'], label='Loss')
     plt.plot(history.history['val_loss'], label='Validation Loss')
     plt.xlabel('Epoche')
