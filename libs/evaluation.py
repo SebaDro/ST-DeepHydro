932bfeaca54330dfc93f6093932f65373cedd29d
Sebastian Drost <s.drost@52north.org>
2022-06-02 13:19:18 +0200

    Make evaluation calculation static

--- libs/evaluation.py
+++ libs/evaluation.py
@@ -5,11 +5,10 @@ from typing import Union
 
 from libs import processing
 
-
 logger = logging.getLogger(__name__)
 
 
-def nse_metric(observations: xr.DataArray, predictions: xr.DataArray, as_dataset: bool = False, basin: str = None)\
+def nse_metric(observations: xr.DataArray, predictions: xr.DataArray, as_dataset: bool = False, basin: str = None) \
         -> Union[float, xr.Dataset]:
     """
     Calculates the Nash-Sutcliffe Efficiency (NSE) metric from streamflow observations and predictions.
@@ -39,6 +38,40 @@ def nse_metric(observations: xr.DataArray, predictions: xr.DataArray, as_dataset
         return nse
 
 
+def calc_evaluation(ds_obs: xr.Dataset, ds_pred: xr.Dataset, target_var: str, basin: str) -> xr.Dataset:
+    """
+    Prepares observations and predictions and calculates evaluation metrics for a certain basin and target variable.
+    Therefore, xarray.Datasets containing observations and predictions are merged to cover the same timespan in a
+    first step. Following, the NSE metric is calculated and the result appended to already existing results
+
+    Parameters
+    ----------
+    ds_obs: xarray.Dataset
+        Dataset containing observations
+    ds_pred: xarray.Dataset
+        Dataset containing predictions
+    target_var: str
+        Name of the target variable, which will be used to create target_obs and target_pred variables in the
+        common dataset.
+    basin: str
+        ID of the basin
+
+    Returns
+    -------
+    xr.Dataset
+        A dataset containing the evaluation results
+
+    """
+    ds_res = processing.merge_observations_and_predictions(ds_obs, ds_pred, True)
+    observations = ds_res[target_var + "_obs"]
+    predictions = ds_res[target_var + "_pred"]
+
+    ds_nse = nse_metric(observations, predictions, as_dataset=True, basin=basin)
+    ds_res = xr.merge([ds_res, ds_nse])
+
+    return ds_res
+
+
 class Evaluation:
 
     def __init__(self):
@@ -48,7 +81,7 @@ class Evaluation:
     def ds_results(self):
         return self.__ds_results
 
-    def append_evaluation_results(self, ds: xr.Dataset):
+    def append_evaluation_results(self, eval):
         """
         Appends new evaluation results to the existing ones
 
@@ -58,48 +91,30 @@ class Evaluation:
             Dataset containing aligned observation and prediction timeseries data as well as basin indexed NSE metrics
 
         """
-        self.__ds_results = xr.merge([self.__ds_results, ds])
+        if isinstance(eval, xr.Dataset):
+            self.__ds_results = xr.merge([self.__ds_results, eval])
+        elif isinstance(eval, Evaluation):
+            self.__ds_results = xr.merge([self.__ds_results, eval.ds_results])
+        else:
+            raise ValueError(f"Unsupported evaluation result type: {type(eval)}.")
 
-    def calc_evaluation(self, ds_obs: xr.Dataset, ds_pred: xr.Dataset, target_var: str, basin: str,
-                        append: bool = True) -> Union[None, xr.Dataset]:
+    def save(self, out_dir: str, pref: str = None) -> str:
         """
-        Prepares observations and predictions and calculates evaluation metrics for a certain basin and target variable.
-        Therefore, xarray.Datasets containing observations and predictions are merged to cover the same timespan in a
-        first step. Following, the NSE metric is calculated and the result appended to already existing results
+        Saves the evaluation result as NetCDF within a specified directory.
 
         Parameters
         ----------
-        ds_obs: xarray.Dataset
-            Dataset containing observations
-        ds_pred: xarray.Dataset
-            Dataset containing predictions
-        target_var: str
-            Name of the target variable, which will be used to create target_obs and target_pred variables in the
-            common dataset.
-        basin: str
-            ID of the basin
-        append: bool
-            Indicates whether the evaluation result should be appended to the existing ones of this instance or returned
-            without appending.
+        out_dir: str
+            Evaluation results will be stored within this directory
+        pref: str
+            An optional prefix, used for the evaluation result file name.
 
         Returns
         -------
-        Union[None, xr.Dataset]
-            None or a dataset containing the evaluation results in accordance to the 'append' parameter
+        str
+            Path to the evaluation result NetCDF file
 
         """
-        ds_res = processing.merge_observations_and_predictions(ds_obs, ds_pred, True)
-        observations = ds_res[target_var + "_obs"]
-        predictions = ds_res[target_var + "_pred"]
-
-        ds_nse = nse_metric(observations, predictions, as_dataset=True, basin=basin)
-        ds_res = xr.merge([ds_res, ds_nse])
-        if append:
-            self.append_evaluation_results(ds_res)
-        else:
-            return ds_res
-
-    def save(self, out_dir: str, pref: str = None) -> str:
         name = "prediction.nc" if pref is None else f"{pref}_prediction.nc"
         out_path = os.path.join(out_dir, name)
         self.ds_results.to_netcdf(out_path)
