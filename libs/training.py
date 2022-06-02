e9b12cfe41ad6fe111334025b76affa86b99b4d1
Sebastian Drost <s.drost@52north.org>
2022-06-02 13:20:38 +0200

    Improve training workflow

--- libs/training.py
+++ libs/training.py
@@ -30,7 +30,7 @@ def run_data_preparation(cfg: config.Config, data_loader: dataloader.HydroDataLo
     ds_test = data_loader.load_single_dataset(cfg.data_config.test_cfg.start_date,
                                               cfg.data_config.test_cfg.end_date, basin)
 
-    logger.info("Preprocess datasets.")
+    logger.info(f"Preprocess datasets using '{type(processor)}'.")
     processor.fit(ds_train)
     ds_train = processor.process(ds_train)
     ds_validation = processor.process(ds_validation)
@@ -39,18 +39,19 @@ def run_data_preparation(cfg: config.Config, data_loader: dataloader.HydroDataLo
     return ds_train, ds_validation, ds_test
 
 
-def run_build_and_compile_model(cfg: config.Config, ds_train, ds_validation, work_dir, basin):
+def run_build_and_compile_model(cfg: config.Config, ds_train, ds_validation, out_dir=None):
     logger.info(f"Build model of type '{cfg.model_config.model_type}'.")
     model = models.factory(cfg.model_config)
     model.build(input_shape=(cfg.model_config.timesteps,) + ds_train.get_input_shape())
-    logger.debug(model.model.summary())
+    model.model.summary(print_fn=logger.debug)
 
-    out_dir = os.path.join(work_dir, basin)
-    logger.info(f"Configure training monitor: output_dir={out_dir}, "
-                f"save_checkpoints={cfg.general_config.save_checkpoints}, "
-                f"save_model={cfg.general_config.save_model}")
-    monitor = monitoring.TrainingMonitor(out_dir, cfg.general_config.save_checkpoints,
-                                         cfg.general_config.save_model)
+    monitor = None
+    if out_dir is not None:
+        logger.info(f"Configure training monitor: output_dir={out_dir}, "
+                    f"save_checkpoints={cfg.general_config.save_checkpoints}, "
+                    f"save_model={cfg.general_config.save_model}")
+        monitor = monitoring.TrainingMonitor(out_dir, cfg.general_config.save_checkpoints,
+                                             cfg.general_config.save_model)
 
     logger.info("Start fitting model to training data...")
     model.compile_and_fit(ds_train, ds_validation, monitor=monitor)
@@ -58,43 +59,61 @@ def run_build_and_compile_model(cfg: config.Config, ds_train, ds_validation, wor
     return model
 
 
-def run_evaluation(model, ds_test, processor, eval_res, basin):
+def run_evaluation(model, ds_test, processor, target_var, basin) -> evaluation.Evaluation:
     logger.info("Start evaluating model...")
     ds_prediction = model.predict(ds_test, basin, as_dataset=True, remove_nan=False)
     result = model.evaluate(ds_test, True, basin)
 
+    # Rescale predictions and observation to the origin uom
     ds_prediction = processor.rescale(ds_prediction)
     ds_observation = processor.rescale(ds_test.timeseries)
 
-    eval_res.calc_evaluation(ds_observation, ds_prediction, "streamflow", basin, True)
+    ds_eval = evaluation.calc_evaluation(ds_observation, ds_prediction, target_var, basin)
+
+    eval_res = evaluation.Evaluation()
     eval_res.append_evaluation_results(result)
-    logger.info("Finished evaluating model.")
+    eval_res.append_evaluation_results(ds_eval)
+    logger.info(f"Finished evaluating model. NSE={eval_res.ds_results.sel(basin=basin).nse.values}")
+    return eval_res
 
 
-def run_training(cfg: config.Config):
-    work_dir = ioutils.create_out_dir(cfg.general_config.output_dir, cfg.general_config.name)
-    logger.info(f"Created working directory '{work_dir}'.")
+def run_training(cfg: config.Config, dry_run):
+    work_dir = None
+    if not dry_run:
+        work_dir = ioutils.create_out_dir(cfg.general_config.output_dir, cfg.general_config.name)
+        logger.info(f"Created working directory '{work_dir}'.")
 
     data_loader = dataloader.HydroDataLoader.from_config(cfg.data_config)
     processor = processing.DefaultDatasetProcessor()
-    eval_res = evaluation.Evaluation()
+    common_eval_res = evaluation.Evaluation()
 
     with open(cfg.data_config.basins_file, 'r') as file:
         basins = [line.strip() for line in file.readlines()]
 
-    logger.info(f"Training will be run for {len(basins)}.")
+    logger.info(f"Training will be run for {len(basins)} basins.")
     logger.debug(f"Basins: {basins}.")
 
     for basin in basins:
-        logger.info(f"Start data preparation for basin {basin}.")
+        logger.info(f"Start training for basin {basin}.")
+        out_dir = None
+        if work_dir is not None:
+            out_dir = os.path.join(work_dir, basin)
         try:
+            logger.info(f"Prepare data for basin {basin}.")
             ds_train, ds_validation, ds_test = run_data_preparation(cfg, data_loader, processor, basin)
-            model = run_build_and_compile_model(cfg, ds_train, ds_validation, work_dir, basin)
-            run_evaluation(model, ds_test, processor, eval_res, basin)
+            model = run_build_and_compile_model(cfg, ds_train, ds_validation, out_dir)
+            eval_res = run_evaluation(model, ds_test, processor, cfg.data_config.streamflow_cfg.variables[0], basin)
+
+            if not dry_run:
+                common_eval_res.append_evaluation_results(eval_res)
+                res_out_path = eval_res.save(out_dir)
+                logger.info(f"Stored evaluation results '{res_out_path}'.")
         except ValueError:
             logging.exception(f"Training stopped for basin {basin} due to non valid timeseries samples.")
         except Exception:
             logging.exception(f"Training stopped for basin {basin} due to an unexpected error that occurred during"
                               f"training.")
-    res_out_path = eval_res.save(cfg.general_config.output_dir)
-    logger.info(f"Stored evaluation results '{res_out_path}'.")
+        logger.info(f"Successfully finished training for basin {basin}.")
+    if not dry_run:
+        res_out_path = common_eval_res.save(cfg.general_config.output_dir)
+        logger.info(f"Stored common evaluation results '{res_out_path}'.")
