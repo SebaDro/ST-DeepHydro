47e825c21b6d8bc3c46403cb39d6dff281cf77a6
Sebastian Drost <s.drost@52north.org>
2022-10-26 09:17:00 +0200

    Make random seed setting compatible for hight TenorFlor versions

--- stdeephydro/utils.py
+++ stdeephydro/utils.py
@@ -1,11 +1,15 @@
 import os
 import tensorflow as tf
 import logging
+import random
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
 
 def setup_reproducibility(seed: int):
     os.environ['PYTHONHASHSEED'] = "0"
-    tf.keras.utils.set_random_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
     logger.info(f"Set random seed to {seed}.")
